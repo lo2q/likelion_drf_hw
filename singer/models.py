e5d64ac6263701e55a4c979a157c07d1a6a32a6b
lo2q <suhyeonkvvon@gmail.com>
2025-07-15 21:47:44 +0900

    Add: singer, song 모델 필드 추가

--- singer/models.py
+++ singer/models.py
@@ -10,6 +10,9 @@ def image_upload_path(instance, filename):
 
 class Singer(models.Model):
     id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, default='임시이름')
+    genre = models.CharField(max_length=50, default='기타')
+    member_count = models.IntegerField(default='1')
     content = models.TextField()
     debut = models.DateTimeField()
     tags = models.ManyToManyField(Tag, blank=True)
@@ -22,5 +25,6 @@ class SingerImage(models.Model):
 class Song(models.Model):
     id = models.AutoField(primary_key=True)
     singer = models.ForeignKey(Singer, on_delete=models.CASCADE, related_name = 'songs')
+    title = models.CharField(max_length=100, default='임시제목')
     release = models.DateTimeField()
     content = models.TextField()
